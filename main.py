3fc5060cd460cf8e7b10c1c09c739a14354add6e
Belal-Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-12-31 10:23:19 +0200

    tested scale invariant

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     color_pencils_template_path = r'data\color_pencils_template.jpeg'
 
     test_template_matching(color_pencils_path, color_pencils_template_path)
-    test_size_flexible_template_matching(rainbow_path, rainbow_template_path, 0.995, False)
+    test_size_flexible_template_matching(rainbow_path, r'data\scaled-rainbow_template.png', 0.995, False)
     test_rotation_flexible_template_matching(rainbow_path, r'data\rotated-rainbow_template.png', 0.999, False)
     test_template_matching(rainbow_path, color_pencils_template_path, 0.6, False)
